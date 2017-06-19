b600a6a0413def3eb7917ccb4253665782766ac2
Hwan Wen Chen <munga0923@gmail.com>
2017-06-20 07:10:10 +0800

    Update production code and make test pass

--- BookShop/BookShop/ShoppingCart.cs
+++ BookShop/BookShop/ShoppingCart.cs
@@ -7,26 +7,31 @@ namespace BookShop
 {
 	public class ShoppingCart
 	{
-		private List<CartItem> _books;
+		private List<CartItem> _items;
 
 		public ShoppingCart()
 		{
-			_books = new List<CartItem>();
+			_items = new List<CartItem>();
 		}
 
 		public void AddBook(CartItem book)
 		{
-			_books.Add(book);
+			_items.Add(book);
 		}
 
 		public void AddMultiBooks(List<CartItem> books)
 		{
-			_books.AddRange(books);
+			_items.AddRange(books);
 		}
 
 		public int GetPrice()
 		{
-			return 100;
+			var price = _items.Sum(item => item.Price);
+			if (_items.Count > 1)
+			{
+				price = (int) (price * 0.95);
+			}
+			return price;
 		}
 	}
 }
